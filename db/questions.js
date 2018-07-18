4d0b20c521d47e951f0054d18fac745b9da30ef4
Adrian Orłów <aorlow@mail.com>
2018-07-18 18:30:13 +0200

    Update v0.1
    Add:
    + Ideologies info
    + Questions database
    + 75% of app frontend

---
+++ db/questions.js
@@ -0,0 +1,54 @@
+questions = [
+    {
+        question: "Sample question 1",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "a1",
+                effect: 3
+            },
+            {
+                axis: "b3",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "b2",
+                effect: 10
+            }
+        ]
+    },
+    {
+        question: "Sample question 2",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "a1",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "b2",
+                effect: 10
+            }
+        ]
+    },
+    {
+        question: "Sample question 3",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "a1",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "b2",
+                effect: 10
+            }
+        ]
+    }
+];
