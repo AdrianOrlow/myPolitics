3dda014f6ba994301602c9ecd5b8f1877058895c
Adrian Orłów <aorlow@mail.com>
2018-07-19 02:09:19 +0200

    Update v0.2
    Add:
    + 21 new questions
    + working results
    + questions counter
    
    Fix:
    * calculating score
    * bars shadow

--- db/questions.js
+++ db/questions.js
@@ -1,6 +1,6 @@
 questions = [
     {
-        question: "Sample question 1",
+        question: "Walka o równość klas społecznych jest priorytetem.",
         answer: 0,
         effectsYes: [
             {
@@ -8,46 +8,490 @@ questions = [
                 effect: 3
             },
             {
-                axis: "b3",
+                axis: "b1",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "a2",
+                effect: 3
+            },
+            {
+                axis: "b2",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Samo istnienie państwa jest zagrożeniem dla wolności.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "c1",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "c2",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Ogół ludzi w obliczu kryzysu nie podejmuje dobrych decyzji.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "b1",
+                effect: 3
+            },
+            {
+                axis: "c2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "d1",
+                effect: 3
+            },
+            {
+                axis: "e2",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Walka z innymi rasami jest priorytetem.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "d2",
+                effect: 3
+            },
+            {
+                axis: "g2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "d1",
+                effect: 3
+            },
+            {
+                axis: "g1",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Osoby w skrajnej biedzie powinny mieć od państwa zapewnione minimum, aby przetrwać.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "b1",
                 effect: 3
             }
         ],
         effectsNo: [
             {
                 axis: "b2",
-                effect: 10
+                effect: 3
             }
         ]
     },
     {
-        question: "Sample question 2",
+        question: "Ochrona środowiska jest bardzo ważna w współczesnym świecie.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "b1",
+                effect: 3
+            },
+            {
+                axis: "e1",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "b2",
+                effect: 3
+            },
+            {
+                axis: "e2",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Nie powinno być żadnych regulacji co do prawa do aborcji.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "c1",
+                effect: 3
+            },
+            {
+                axis: "f1",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "c2",
+                effect: 3
+            },
+            {
+                axis: "f2",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Pornografia powinna być zdelegalizowana.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "b1",
+                effect: 3
+            },
+            {
+                axis: "c2",
+                effect: 3
+            },
+            {
+                axis: "f2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "b2",
+                effect: 3
+            },
+            {
+                axis: "c1",
+                effect: 3
+            },
+            {
+                axis: "f1",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Podatki to kradzież",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "b2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "b1",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Ustrój państwa powinien być oparty o zasady szerokiej decentralizacji",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "c1",
+                effect: 3
+            },
+            {
+                axis: "b2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "b1",
+                effect: 3
+            },
+            {
+                axis: "c2",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Kodeks Pracy powinien zostać zniesiony lub mocno ograniczony.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "a2",
+                effect: 3
+            },
+            {
+                axis: "b2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "a1",
+                effect: 3
+            },
+            {
+                axis: "b1",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Nie można ograniczać wolności działania ludzi, jeśli nie wykażemy, że są one faktycznie szkodliwe społecznie.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "a2",
+                effect: 3
+            },
+            {
+                axis: "b2",
+                effect: 3
+            },
+            {
+                axis: "e2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "a1",
+                effect: 3
+            },
+            {
+                axis: "b1",
+                effect: 3
+            },
+            {
+                axis: "c2",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Fizyka, geometria, i różne inne systemy matematyczne wydają się aż nazbyt perfekcyjnie oddawać rzeczywistość świata. Myśl precyzyjnie oddaje świat dlatego, że rzeczywistość świata jest rzeczywistością naszego umysłu.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "f1",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "f2",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Upaństwawianie niektórych sektorów gospodarki jest korzystne",
         answer: 0,
         effectsYes: [
             {
                 axis: "a1",
                 effect: 3
+            },
+            {
+                axis: "c2",
+                effect: 3
             }
         ],
         effectsNo: [
             {
-                axis: "b2",
-                effect: 10
+                axis: "a2",
+                effect: 3
             }
         ]
     },
     {
-        question: "Sample question 3",
+        question: "Wolny rynek zawsze korzystnie wpływa na gospodarkę",
         answer: 0,
         effectsYes: [
             {
+                axis: "a2",
+                effect: 3
+            },
+            {
+                axis: "b2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
                 axis: "a1",
+                effect: 3
+            },
+            {
+                axis: "b1",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Związki partnerskie pomiędzy osobami tej samej płci powinny być legalne",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "b2",
+                effect: 3
+            },
+            {
+                axis: "e2",
+                effect: 3
+            },
+            {
+                axis: "f1",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "b1",
+                effect: 3
+            },
+            {
+                axis: "f2",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Symbole narodowe mają dużą wartość",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "f2",
+                effect: 3
+            },
+            {
+                axis: "g2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "g1",
+                effect: 3
+            },
+            {
+                axis: "f1",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Najważniejsze jest wychowanie zgodnie z tradycjami, nawet jeśli zaburzają punkt widzenia dziecka na współczesny świat.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "f2",
                 effect: 3
             }
         ],
         effectsNo: [
             {
                 axis: "b2",
-                effect: 10
+                effect: 3
+            },
+            {
+                axis: "e2",
+                effect: 3
+            },
+            {
+                axis: "f1",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Jeśli ktoś nie jest wierzy w mojego Boga, oznacza to, że nie ma dobrych wartości moralnych.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "f2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "b2",
+                effect: 3
+            },
+            {
+                axis: "e2",
+                effect: 3
+            },
+            {
+                axis: "f1",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Waluta z parytetem złota jest lepsza od pieniądza fiducjarnego.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "a2",
+                effect: 3
+            },
+            {
+                axis: "f2",
+                effect: 3
+            },
+            {
+                axis: "b2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "b1",
+                effect: 3
+            },
+            {
+                axis: "c2",
+                effect: 3
+            },
+            {
+                axis: "f1",
+                effect: 3
+            }
+        ]
+    },
+    {
+        question: "Władza musi pochodzić od Boga i być zakorzeniona w tradycji, więc należy się tylko najstarszym rodom arystokratycznym.",
+        answer: 0,
+        effectsYes: [
+            {
+                axis: "c2",
+                effect: 3
+            },
+            {
+                axis: "f2",
+                effect: 3
+            }
+        ],
+        effectsNo: [
+            {
+                axis: "c1",
+                effect: 3
+            },
+            {
+                axis: "f1",
+                effect: 3
             }
         ]
     }
